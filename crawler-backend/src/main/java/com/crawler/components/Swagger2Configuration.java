aa766c499a5d6bd4444c384273be81e8f3d6b5a7
yinbo <yinbo0527>
2017-11-09 10:28:27 +0800

    更新swagger2文档描述

--- crawler-backend/src/main/java/com/crawler/components/Swagger2Configuration.java
+++ crawler-backend/src/main/java/com/crawler/components/Swagger2Configuration.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,9 +30,9 @@ public class Swagger2Configuration {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("使用Swagger2构建RESTful APIs")
-                .description("测试描述：from crawler")
-                .contact("crawler")
+                .title("crawler-plus后台Restful APIs")
+                .description("written by crawler")
+                .contact(new Contact("crawler", "https://github.com/crawler-plus/crawler-plus", "yinbo0527@126.com"))
                 .version("1.0")
                 .build();
     }
