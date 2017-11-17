1f45b5e21a426ce07bc2ba41eccf32fc2811cf20
yinbo <yinbo0527>
2017-11-17 14:58:53 +0800

    修改工程中的一些错误

--- crawler-config-server/src/main/java/com/crawler/CrawlerConfigServerApplication.java
+++ crawler-config-server/src/main/java/com/crawler/CrawlerConfigServerApplication.java
@@ -1,8 +1,5 @@
 package com.crawler;
 
-import com.crawler.util.LoggerUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,12 +9,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class CrawlerConfigServerApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(CrawlerConfigServerApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(CrawlerConfigServerApplication.class);
 		application.setBannerMode(Banner.Mode.OFF);
 		application.run(args);
-		LoggerUtils.printLogger(logger, "--------------crawler-config-server project starts----------------");
 	}
 }
