ba53a1e3e2be4fec2d4bc2cdc30c159b4bdd4bbb
yinbo <yinbo0527>
2017-11-20 17:33:43 +0800

    替换escape和unescape为encodeURIComponent和decodeURIComponent

--- crawler_frontend/common/commonUtil.js
+++ crawler_frontend/common/commonUtil.js
@@ -148,12 +148,11 @@ var commonUtil = function () {
     /**
      * 获取url中的参数
      * @param name
-     * @returns {null}
      */
     var _getUrlParam = function (name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
         var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-        if (r != null) return unescape(r[2]);
+        if (r != null) return decodeURIComponent(r[2]);
         return null; //返回参数值
     };
 
@@ -167,9 +166,9 @@ var commonUtil = function () {
         for (var key in obj) {
             var value = obj[key];
             if (url.indexOf("?") > -1) {
-                url += "&" + key + "=" + escape(value);
+                url += "&" + key + "=" + encodeURIComponent(value);
             } else {
-                url += "?" + key + "=" + escape(value);
+                url += "?" + key + "=" + encodeURIComponent(value);
             }
         }
         return url;
