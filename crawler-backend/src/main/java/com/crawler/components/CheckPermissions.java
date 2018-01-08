4d5a885925e069f473fd3bbbb552a3f332d96e9f
yinbo <yinbo0527@126.com>
2018-01-08 18:17:35 +0800

    1.部分语法改成jdk1.8的stream方式 2.将hutool maven版本还原成3.2.2,因为3.3.1的版本生成验证码有问题 3.zuul server的配置中将hystrix的超时配置关闭，否则debug服务接口时很快就会出现TimeoutException

--- crawler-backend/src/main/java/com/crawler/components/CheckPermissions.java
+++ crawler-backend/src/main/java/com/crawler/components/CheckPermissions.java
@@ -3,13 +3,14 @@ package com.crawler.components;
 import com.crawler.domain.SysMenu;
 import com.crawler.exception.SecurityException;
 import com.crawler.service.api.MenuService;
-import com.google.common.collect.Lists;
 import com.google.common.primitives.Ints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * 验证用户权限公共类
@@ -30,17 +31,16 @@ public class CheckPermissions {
         List<Integer> permissionsList = Ints.asList(permissionValue);
         // 根据用户id得到某个用户的权限
         List<SysMenu> menuList = menuService.getMenuList(Integer.parseInt(uid));
-        // 装这个用户可以访问的菜单id列表
-        List<Integer> availableMenuList = Lists.newArrayList();
         if(!CollectionUtils.isEmpty(menuList)) {
-            for (SysMenu sysMenu : menuList) {
-                availableMenuList.add(sysMenu.getMenuId());
+            // 这个用户可以访问的菜单id列表
+            List<Integer> availableMenuList = menuList.stream().map(SysMenu::getMenuId).collect(toList());
+            // 求交集
+            availableMenuList.retainAll(permissionsList);
+            // 表明这个用户没有注解中标明的权限
+            if(CollectionUtils.isEmpty(availableMenuList)) {
+                throw new SecurityException("insecurity access");
             }
-        }
-        // 求交集
-        availableMenuList.retainAll(permissionsList);
-        // 表明这个用户没有注解中标明的权限
-        if(CollectionUtils.isEmpty(availableMenuList)) {
+        }else {
             throw new SecurityException("insecurity access");
         }
     }
