d4e50ef9ab94cebaff70eef20c802bd557578a03
yinbo <yinbo0527@126.com>
2018-01-09 09:49:26 +0800

    删除定时根据模板更新文章功能和定时爬取债券信息功能

--- crawler-backend/src/main/java/com/crawler/components/JobsConfiguration.java
+++ crawler-backend/src/main/java/com/crawler/components/JobsConfiguration.java
@@ -1,9 +1,5 @@
 package com.crawler.components;
 
-import com.crawler.domain.SysLock;
-import com.crawler.service.api.ArticleService;
-import com.crawler.service.api.BondMarketService;
-import com.crawler.service.api.SysLockService;
 import com.crawler.util.LoggerUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -13,10 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * 定时任务
@@ -26,36 +20,9 @@ import java.util.List;
 public class JobsConfiguration {
 
     @Autowired
-    private ArticleService articleService;
-
-    @Autowired
-    private SysLockService sysLockService;
-
-    @Autowired
     private CrawlerProperties crawlerProperties;
 
-    @Autowired
-    private BondMarketService bondMarketService;
-
     private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    // 每隔4个小时定时跑一次定时任务，根据网站配置模版抓取网页信息
-    @Scheduled(cron = "0 0 10,14,18 * * ?")
-    public void cronJobForCrawlerContent() {
-        // 当网站正在运行模版定时任务的时候，向数据库中写入值
-        SysLock sysLock = new SysLock();
-        sysLock.setSystemCron("1");
-        sysLockService.updateSysLock(sysLock);
-        List<Integer> allUserId = articleService.fetchAllUserIdFromTemplateConfig();
-        if(!CollectionUtils.isEmpty(allUserId)) {
-            for (Integer id : allUserId) {
-                this.articleService.cronjob(id);
-            }
-        }
-    	// 当网站运行模版定时任务结束后，将值从数据库中移除
-        sysLock.setSystemCron("0");
-        sysLockService.updateSysLock(sysLock);
-    }
 
     /**
      * 清除ftp服务器上的验证码图片,每天2点和14点定时清理
@@ -81,11 +48,5 @@ public class JobsConfiguration {
                 LoggerUtils.printExceptionLogger(logger, e);
             }
         }
-    }
-
-    // 每隔4个小时定时跑一次定时任务，抓取债券市场信息
-    @Scheduled(cron = "0 0 10,14,18 * * ?")
-    public void cronJobForBondMarket() {
-        bondMarketService.crawBondMarket();
     }
 }
