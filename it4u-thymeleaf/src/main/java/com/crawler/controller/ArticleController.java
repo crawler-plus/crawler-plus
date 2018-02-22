984c7fa1344ed7404e7bda956aded3a9cb202577
yinbo <yinbo0527@126.com>
2018-02-22 17:35:51 +0800

    1.thymeleaf工程加缓存 2.删除部分无用的pom依赖 3.修正某些bug

--- it4u-thymeleaf/src/main/java/com/crawler/controller/ArticleController.java
+++ it4u-thymeleaf/src/main/java/com/crawler/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.crawler.controller;
 
+import com.crawler.cache.RedisCacheHandler;
 import com.crawler.domain.ArticleTransferEntity;
 import com.crawler.domain.BaseEntity;
 import com.crawler.domain.CrawlerContent;
@@ -8,10 +9,16 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.crawler.constant.ResponseCodeConst.MESSAGE_CODE_OK;
 
@@ -24,6 +31,12 @@ public class ArticleController {
 	@Autowired
 	private ArticleService articleService;
 
+	@Autowired
+	private RedisCacheHandler redisCacheHandler;
+
+	@Autowired
+	private ThymeleafViewResolver thymeleafViewResolver;
+
 	@GetMapping(value = "/")
 	public String hello() {
 		return "index";
@@ -35,6 +48,7 @@ public class ArticleController {
 	@PostMapping(value = "/fetchArticles")
 	@ResponseBody
 	public BaseEntity listAllSimpleCrawlerContents(@RequestBody ArticleTransferEntity at, BaseEntity be) {
+		// TODO 需要从elasticsearch中检索文章
 		// 默认一页显示10条数据
 		int limit = 10;
 		// 将输入关键字变成小写
@@ -58,14 +72,37 @@ public class ArticleController {
 	/**
 	 * 根据id获得指定文章
 	 */
-	@GetMapping(path = "/article/{id}")
-	public ModelAndView getCrawlerContent(@PathVariable("id") int id, BaseEntity be) {
+	@GetMapping(path = "/article/{id:\\d+}", produces = "text/html")
+	@ResponseBody
+	public String getCrawlerContent(@PathVariable("id") int id
+			, BaseEntity be, HttpServletRequest request, HttpServletResponse response,
+			 Model model) {
+		// 取缓存
+		String article = (String)redisCacheHandler.getCache("article:" + id);
+		if(!StringUtils.isEmpty(article)) {
+			return article;
+		}
+		// 去数据库查文章
 		CrawlerContent crawlerContent = articleService.getCrawlerContent(id);
+		// 如果查询不到文章
+		if(crawlerContent == null) {
+			// 将不存在的文章也写入缓存一个值，防止被恶意刷
+			redisCacheHandler.setCacheWithTimeout("article:" + id, "unknown", 30L, TimeUnit.MINUTES);
+			return "unknown";
+		}
+		// 走到这里表明查到了文章，将文章内容设置到model中
 		be.setContent(crawlerContent);
-		be.setMsgCode(MESSAGE_CODE_OK.getCode());
-		ModelAndView mav = new ModelAndView("articleDetail");
-		mav.addObject("messages", be);
-		return mav;
+		model.addAttribute("messages", be);
+		WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale(),
+				model.asMap());
+		// 手动渲染
+		article = thymeleafViewResolver.getTemplateEngine().process("articleDetail",
+				ctx);
+		// 写缓存
+		if(!StringUtils.isEmpty(article)) {
+			redisCacheHandler.setCacheWithTimeout("article:" + id, article, 30L, TimeUnit.DAYS);
+		}
+		return article;
 	}
 
 	@GetMapping(path = "/about")
